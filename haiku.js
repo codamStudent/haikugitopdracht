cb271d97cdfa3e571febb01c04649f7a60a41e83
Samuel <you@example.comsamuel@enderink.com>
2023-10-09 10:18:57 +0200

    dit is he highq

--- haiku.js
+++ haiku.js
@@ -1,4 +1,11 @@
 
+function Haiku2(){
+    console.log("haiku by:","samuel");
+    console.log("one for the money");
+    console.log("two for the better green");
+    console.log("3 for the methylenedioxymethamphetamine");
+}
+
 
 //plak hier de haiku functions onder elkaar
 function Haiku1() {
@@ -8,7 +15,7 @@ function Haiku1() {
 
 
 
-//oaat de code hieronder met rust
+//laat de code hieronder met rust
 Haiku1()
 Haiku2()
 Haiku3()
