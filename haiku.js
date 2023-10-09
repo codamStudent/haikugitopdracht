5fae9204654d2c89721371e6e623cdb0d55fce0d
codamStudent <37171@ma-web.nl>
2023-10-09 09:46:19 +0200

    haiku1

--- haiku.js
+++ haiku.js
@@ -1,11 +1,14 @@
 
 
-#plak hier de haiku functions onder elkaar
+//plak hier de haiku functions onder elkaar
+function Haiku1() {
+    console.log("The wren\nEarns his living\n\
+        Noiselessly.")
+}
 
 
 
-
-#laat de code hieronder met rust
+//oaat de code hieronder met rust
 Haiku1()
 Haiku2()
 Haiku3()
